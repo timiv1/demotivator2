f7122aba0aa4a0c947d4b987a71a655dca8fe103
timi vovk <timi.vovk@gmail.com>
2023-05-13 19:14:34 +0200

    Real programmers don't have to debug!!

--- extension.js
+++ extension.js
@@ -11,9 +11,10 @@ const vscode = require('vscode');
 function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "demotivator2" is now active!');
+		// console.log("Real programmers don't have to debug");
 
+	// This line of code will only be executed once when your extension is activated
+	vscode.window.showInformationMessage("Real programmers don't have to debug");
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
